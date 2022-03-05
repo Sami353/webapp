cf4c6000596d2bf732c1bff9e97bb8bc43a597cb
Sami353 <82566175+Sami353@users.noreply.github.com>
2022-03-05 14:15:43 +0545

    saving changes on git

--- src/App.js
+++ src/App.js
@@ -7,24 +7,29 @@ import {
   Route,
   Switch
 } from "react-router-dom";
-// import UserList from "./pages/userList/UserList";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <Router>
+    // <Router>
+    //   <Topbar />
+    //   <div className="container">
+    //     <Sidebar />
+    //     <Switch>
+    //       <Route exact path="/">
+    //         <Home />
+    //       </Route>
+    //       <Route path="/users">
+    //         <UserList />
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+    <div>
       <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          {/* <Route path="/users">
-            <UserList />
-          </Route> */}
-        </Switch>
-      </div>
-    </Router>
+      <Sidebar />
+      <Home />
+    </div>
   );
 }
 
