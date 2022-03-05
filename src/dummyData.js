b2db82fa861189f2ace27964617565cdaf885cd4
Sami353 <82566175+Sami353@users.noreply.github.com>
2022-03-05 13:50:28 +0545

    changes

--- src/dummyData.js
+++ src/dummyData.js
@@ -48,3 +48,86 @@ export const userData = [
         "Active User": 3000,
     }
 ];
+
+export const userRows = [
+    {
+        id: 1,
+        username: 'Jenny Walker',
+        avatar: "https://th.bing.com/th/id/R.a424d10440dc2cb7cc2ec1c93b3b63b9?rik=MjLwYPp6Ru7pJw&pid=ImgRaw&r=0",
+        email: "jenny@gmail.com",
+        status: "Active",
+        transaction: "$190.00"
+    },
+    {
+        id: 2,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 3,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 4,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 5,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 6,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 7,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 8,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 9,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    },
+    {
+        id: 10,
+        username: 'John Snow',
+        avatar: "https://www.rpidatang.com/static/dashboard/assets/img/avatar.png",
+        email: "john@gmail.com",
+        status: "Active",
+        transaction: "$120.00"
+    }
+];
