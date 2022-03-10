7de7bcb964b56d31264fb4b87bfad603eca4bcf2
Sami353 <82566175+Sami353@users.noreply.github.com>
2022-03-10 13:39:52 +0545

    saving on git

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,28 +1,63 @@
 import { Link } from "react-router-dom"
-import { Publish } from "@material-ui/icons"
+// import { Publish } from "@material-ui/icons"
 import "./user.css"
 
 export default function user() {
     return (
         <div className="user">
             <div className="userTitleContainer">
-                <h1 className="userEditTitle">Edit User</h1>
+                <h1 className="userEditTitle">Edit Customers</h1>
                 <Link to="/newUser">
                     <button className="userAddButton">Create</button>
                 </Link>
             </div>
             <div className="userContainer">
                 <div className="userUpdate">
-                    <span className="userUpdateTitle">Edit</span>
                     <form className="userUpdateForm">
                         <div className="userUpdateLeft">
-                            <div className="userUpdateItem">
-                                <label>Username</label>
-                                <input type="text" placeholder="annabeck998" className="userUpdateInput" />
+                            <div className="userUpdateRight">
+                                <span className="userUpdateUpload">
+                                    <img src="https://i.pinimg.com/originals/89/fd/79/89fd79d35dbcfe7fd70bf5f7c159f56c.jpg" alt="" className="userUpdateImg" />
+                                </span>
+                                <span className="userImgUploadButton">
+                                    <label htmlFor="file">
+                                        {/* <Publish className="userUpdateIcon" /> */}
+                                        <div className="userImageUploadBtn">
+                                            <span className="imgUploadBtnUpload">
+                                                {/* <button>Upload New Image</button> */}
+                                                Upload New Image
+                                            </span>
+                                            <span className="ImgUploadBtnRemove">
+                                                Remove
+                                            </span>
+                                        </div>
+                                        <div className="userImageDescriptn">
+                                            Image should be at least 400 x 400px as a png or jpeg file.
+                                        </div>
+                                    </label>
+                                    <input type="file" id="file" style={{ display: "none" }} />
+                                </span>
                             </div>
-                            <div className="userUpdateItem">
-                                <label>Full name</label>
-                                <input type="text" placeholder="Anna Becker" className="userUpdateInput" />
+                            <h3 className="userHeading">Enter your details below:</h3>
+                            <div className="userItemNameContainer">
+                                <div className="userUpdateItemName">
+                                    <label>First name</label>
+                                    <input type="text" placeholder="Anna" className="userUpdateInputName" />
+                                </div>
+                                <div className="userUpdateItemName">
+                                    <label>Last Name</label>
+                                    <input type="text" placeholder="Becker" className="userUpdateInputName" />
+                                </div>
+                            </div>
+                            <div className="userItemNameContainer">
+                                <div className="userUpdateItemName">
+                                    <label>Full name</label>
+                                    <input type="text" placeholder="Anna Becker" className="userUpdateInputName" />
+                                </div>
+                                <div className="userUpdateItemName">
+                                    <label>Username</label>
+                                    <input type="text" placeholder="annabeck998" className="userUpdateInputName" />
+                                </div>
                             </div>
                             <div className="userUpdateItem">
                                 <label>Email</label>
@@ -37,15 +72,6 @@ export default function user() {
                                 <input type="text" placeholder="Putalisadak, Ktm" className="userUpdateInput" />
                             </div>
                             <div className="userUpdateRight">
-                                <div className="userUpdateUpload">
-                                    <img src="https://th.bing.com/th/id/OIF.Yroca081XInwFEO0xjw6Cw?pid=ImgDet&rs=1" alt="" className="userUpdateImg" />
-                                </div>
-                                <div className="userImgUploadButton">
-                                    <label htmlFor="file">
-                                        <Publish className="userUpdateIcon" />
-                                    </label>
-                                    <input type="file" id="file" style={{ display: "none" }} />
-                                </div>
                                 <button className="userUpdateButton">Update</button>
                             </div>
                         </div>
